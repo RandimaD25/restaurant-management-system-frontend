61ebfd13d3230d9302045394eb8eefa2cbd0fb1a
RandimaD25 <randimamethminid525@gmail.com>
2025-04-05 13:32:52 +0530

    feat: create popular-with-other-people-section

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import CategoryNavigationComponent from "@/components/category-navigation";
+import PopularWithOtherPeopleSection from "@/components/popular-with-other-people-section";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,6 +30,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <CategoryNavigationComponent />
+        <PopularWithOtherPeopleSection />
         {children}
       </body>
     </html>
